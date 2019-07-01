9d6327bcf8cdbfe8cf3bcfc07da18c687d75ff9b
志建 <448781578@qq.com>
2019-07-01 10:27:02 +0800

    x

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/test?charset=utf8mb4'  # noqa
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b2bdev:Dev!2345@/120.26.50.66/weixin?charset=utf8mb4'  # noqa
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-REDIS_URL = 'redis://localhost:6379'
+REDIS_URL = 'redis://120.26.50.66:6379'
 SESSION_TYPE = 'redis'
 avatar_tmpl = '/static/img/avatars/{}.jpg'
 UPLOAD_FOLDER = os.path.join(HERE, 'uploads')
